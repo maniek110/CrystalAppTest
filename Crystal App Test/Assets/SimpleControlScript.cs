f740f52c37c104964c252eb44da9f204787b574f
Suchy <marcinsuchora@gmail.com>
2020-07-06 05:19:40 +0200

    Movement update and Traps
    DONE:
    - Updated Movement
    - Added Traps
    - Extended Map

--- Crystal App Test/Assets/SimpleControlScript.cs
+++ Crystal App Test/Assets/SimpleControlScript.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleControlScript : MonoBehaviour
 {
+    public Text timeUI;
+    public Text dead;
+    public Text deathUI;
     public Camera camera;
+    public Transform RespawnPoint;
     public float moveSpeed = 0.1f;
     public float JumpPow = 100f;
     float SmoothAnglStep=0.1f;
@@ -13,47 +18,53 @@ public class SimpleControlScript : MonoBehaviour
     Rigidbody myRigidbody;
     GameObject myGameObject;
     float horizontal, vertical;
-    //Animator animator;
     public bool isAlive = true;
+    float time = 0;
+    int death;
     bool isGround;
-    public LayerMask layerMask;
     // Start is called before the first frame update
     void Start()
     {
         myGameObject = GetComponent<GameObject>();
         myRigidbody = GetComponent<Rigidbody>();
-        //animator = GetComponent<Animator>();
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //CameraRotation();
+        
+        DisplayValues();
         if (isAlive)
         {
-            Movement();
             CheckIfAlive();
+            CountTime();
+            Movement();   
         }
         else {
-            TemporaryReset();   
+            TemporaryReset();  
         }
-        //Debug.Log(vertical);
-        //Debug.Log(myRigidbody.velocity.y);
-        Debug.Log(isGround);
-        Debug.Log(isAlive);
     }
 
     private void TemporaryReset()
     {
-        if (Input.GetKeyDown(KeyCode.R)) isAlive = true;
+        if (Input.GetKeyDown(KeyCode.R)) {
+            isAlive = true;
+            dead.enabled = false;
+            time = 0;
+            transform.position = RespawnPoint.position;
+        } 
     }
 
     private void CheckIfAlive()
     {
+
         if (myRigidbody.velocity.y < -10 && isGround) {
             isAlive = false;
+            dead.enabled = true;
+            death++;
         }
+        
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -61,7 +72,12 @@ public class SimpleControlScript : MonoBehaviour
         if (collision.contacts.Length > 0)
         {
             ContactPoint contact = collision.contacts[0];
-            if (Vector3.Dot(contact.normal, Vector3.down) > 0.5) isGround = false;
+            if (Vector3.Dot(contact.normal, Vector3.down) > 0.3) isGround = false;
+        }
+        if (collision.gameObject.tag == "Trap") {
+            isAlive = false;
+            dead.enabled = true;
+            death++;
         }
     }
     private void OnCollisionStay(Collision collision)
@@ -73,18 +89,24 @@ public class SimpleControlScript : MonoBehaviour
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
         Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
-        Debug.Log(dir.x + "|" + dir.y + "|" + dir.z);
+        float tempAngl = Mathf.SmoothDampAngle(transform.eulerAngles.y, camera.transform.eulerAngles.y, ref SmoothAnglVelocity, SmoothAnglStep);
+        transform.rotation = Quaternion.Euler(0, tempAngl,0);
         if (dir.magnitude >= 0.1f) {
-            float angl = Mathf.Atan2(dir.x,dir.z)*Mathf.Rad2Deg+camera.transform.eulerAngles.y;
-            float tempAngl = Mathf.SmoothDampAngle(transform.eulerAngles.y, angl, ref SmoothAnglVelocity, SmoothAnglStep);
-            transform.rotation = Quaternion.Euler(0,tempAngl,0);
-            Vector3 MoveDir = Quaternion.Euler(0f, angl, 0f)*Vector3.forward;
+            Vector3 MoveDir = Quaternion.Euler(0, camera.transform.eulerAngles.y,0)*dir;
             transform.Translate(MoveDir*moveSpeed*Time.deltaTime,Space.World);
         }
         if (Input.GetKey(KeyCode.Space)&&isGround)
         {
             isGround = false;
-            myRigidbody.AddForce(Vector3.up*JumpPow,ForceMode.Impulse);
+            myRigidbody.velocity+=Vector3.up*JumpPow*Time.deltaTime;
         }
+    }
+    void CountTime() {
+        time += Time.deltaTime;
+    }
+    void DisplayValues() {
+        timeUI.text = "TIME: " + time;
+        deathUI.text = "DEATHS: " + death;
+
     }
 }
